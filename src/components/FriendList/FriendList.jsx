5d15d54a61a953fa9b90af04124fb79120703290
Dimentiy88 <bodartinov88@gmail.com>
2023-09-18 20:38:41 +0200

    Apdate-FriedList

---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,17 @@
+import { FriendListItem } from './FriendListItem';
+import css from './FriendList.module.css';
+
+export const FriendList = ({ friends }) => {
+  return (
+    <ul className={css.friendList}>
+      {friends.map(({ avatar, name, isOnline, id }) => (
+        <FriendListItem
+          avatar={avatar}
+          name={name}
+          isOnline={isOnline}
+          key={id}
+        ></FriendListItem>
+      ))}
+    </ul>
+  );
+};
